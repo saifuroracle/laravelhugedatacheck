4b1ab310201e8eedb5c5c4607b292e333244c09e
saifur rahman <saifuroracle@gmail.com>
2021-05-06 00:40:32 +0600

    1 lac data loading complete

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Validator;
+
+
+class ReportController extends Controller
+{
+
+    public function hugedatacheck()
+    {
+        $report = DB::table('students')->get();
+
+        return datatables()->of($report)->make(true);
+    }
+
+
+
+}
